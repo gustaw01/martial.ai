6b961fc28fce2b4ff459322ae15ce5cf85426050
Mateusz Michalik <mateusz.miszalik@wp.pl>
2024-11-24 18:02:11 +0100

    Basic components and basic ui

---
+++ frontend/src/app/component/navigation/navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-navigation',
+  standalone: true,
+  imports: [RouterLink, MatToolbarModule, MatButtonModule],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.scss'
+})
+export class NavigationComponent {
+
+}
