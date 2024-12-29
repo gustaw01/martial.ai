6a946f55d86684bff851bbd4c941a0e7cf74ebba
Mateusz Michalik <mateusz.miszalik@wp.pl>
2024-12-29 03:05:14 +0100

    Simple UI. Redux.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './components/Home'
+import Login from './features/auth/Login'
 import './App.css'
+import DashLayout from './components/DashLayout'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/" element={ <Layout /> }>
+        <Route index element={ <Home /> } />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dash">
+          <Route index element={ <DashLayout /> } />
+        </Route>
+      </Route>
+    </Routes>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
 import './index.css'
 import App from './App.jsx'
 
+import { store } from './app/store'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/*" element={<App />} />
+          </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
