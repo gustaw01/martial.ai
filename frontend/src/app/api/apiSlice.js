17e855f04d3c9ccaddb096ae8da2c9c48e79c712
Mateusz Michalik <mateusz.miszalik@wp.pl>
2025-01-15 22:26:34 +0100

    Plagiarism Assessment slice. Update example db

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -2,6 +2,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
-    tagTypes: ['History'],
+    tagTypes: ['History', 'PlagiarismAssessment'],
     endpoints: (builder) => ({}),
 })
