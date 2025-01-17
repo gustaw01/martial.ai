0abbd085147a6bfc48a16d041dcca48947efdeb1
Mateusz Michalik <mateusz.miszalik@wp.pl>
2025-01-17 21:16:47 +0100

    Added .env

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -1,7 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+    baseQuery: fetchBaseQuery({ baseUrl: apiUrl }),
     tagTypes: ['History', 'PlagiarismAssessment'],
     endpoints: (builder) => ({}),
 })
