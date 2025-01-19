2edc68d0a66003de29037ddae027c9cb4dbda8d3
Mateusz Michalik <mateusz.miszalik@wp.pl>
2025-01-19 01:58:33 +0100

    Fix loading style

--- frontend/src/features/History/HistoryView.jsx
+++ frontend/src/features/History/HistoryView.jsx
@@ -20,7 +20,7 @@ const HistoryView = () => {
         content = (
             <div className="hero bg-base-200 min-h-screen">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <div className=" w-full max-w-sm shrink-0 shadow-2xl">
                         <Loading />
                     </div>
                 </div>
